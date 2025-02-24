114c29c15fd23ae3dcfe51cb449418352bc8230d
thmsgo18 <134527237+thmsgo18@users.noreply.github.com>
2025-02-24 20:03:27 +0100

    Début du code

---
+++ Code/Code.R
@@ -0,0 +1,8 @@
+data <- read.csv("Data/Binome3/haberman.data", header = FALSE, sep = ",")
+colnames(data) <- c("Age du patient", "Annee de l'operation -1900",
+                    "Nombre de ganglions axillaires positifs detectes",
+                    "Statut de survie")
+
+str(data)
+summary(data)
+head(data)
