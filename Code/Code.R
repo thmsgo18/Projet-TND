cc4611c6785057430dca941d71201b82b89c669d
thmsgo18 <134527237+thmsgo18@users.noreply.github.com>
2025-02-26 19:59:44 +0100

    V2

--- Code/Code.R
+++ Code/Code.R
@@ -3,8 +3,11 @@ colnames(data) <- c("Age du patient", "Annee de l'operation -1900",
                     "Nombre de ganglions axillaires positifs detectes",
                     "Statut de survie")
 
-str(data)
-summary(data)
 head(data)
-colSums(is.na(data))
 boxplot(data, main = "Détection des valeurs aberrantes")
+
+colSums(is.na(data))
+
+str(data)
+
+summary(data)
