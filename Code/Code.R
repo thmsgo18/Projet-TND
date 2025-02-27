b9bacb10d3a4d5562d1dd37b03b3164833046b6f
thmsgo18 <134527237+thmsgo18@users.noreply.github.com>
2025-02-27 13:21:20 +0100

    V3

--- Code/Code.R
+++ Code/Code.R
@@ -1,13 +1,15 @@
+# Extarction des datas
 data <- read.csv("Data/Binome3/haberman.data", header = FALSE, sep = ",")
+# Nommage des différentes colonnes
 colnames(data) <- c("Age du patient", "Annee de l'operation -1900",
                     "Nombre de ganglions axillaires positifs detectes",
                     "Statut de survie")
 
 head(data)
-boxplot(data, main = "Détection des valeurs aberrantes")
-
+# Vérification du manque de donnée
 colSums(is.na(data))
-
+# Vérification du typage des variables
 str(data)
-
+# Vérification des valeurs abérrantes
 summary(data)
+
